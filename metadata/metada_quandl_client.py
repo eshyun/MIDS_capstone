60bfa5f93d6c1105f6642e491756d3a3c671e90b
Natarajan Shankar <shankar@chariotnetworks.com>
2017-10-08 07:17:06 -0700

    Adding revenue API

--- metadata/metada_quandl_client.py
+++ metadata/metada_quandl_client.py
@@ -4,6 +4,16 @@ from urllib.request import urlopen
 r = urllib.request.urlopen('https://www.quandl.com/api/v3/datasets/EOD/CSCO.csv?api_key=xR-TQ5sb4NnAa2AtX1TG')
 lines = r.readlines()
 
-for line in lines:
-	print (line)
+with open("CSCO_stock_price.txt", "w+") as fw:
+	for line in lines:
+		fw.write(str(line) + "\n")
 
+
+# for revenue
+revenue = urllib.request.urlopen('https://www.quandl.com/api/v3/datatables/SHARADAR/SF1.csv?ticker=CSCO&qopts.columns=ticker,dimension,datekey,revenue&api_key=xR-TQ5sb4NnAa2AtX1TG')
+
+lines = revenue.readlines()
+
+with open("CSCO_revenue.txt", "w+") as fr:
+	for line in lines:
+		fr.write(str(line) + "\n")
