0998bc9ccfbf9350559fd226c6b6e60666b9e6e3
Natarajan Shankar <shankar@chariotnetworks.com>
2017-10-20 11:48:18 -0700

    Many new updates

--- metadata/example.py
+++ metadata/example.py
@@ -2,7 +2,13 @@ import md_intrinio_client
 
 from md_intrinio_client import intrinio_get_company_metadata
 from md_intrinio_client import intrinio_get_company_financials
+from md_intrinio_client import intrinio_get_company_financials_csv
+from md_intrinio_client import get_SandP_metadata
+from md_intrinio_client import test_SandP_metadata
 
+
+#get_SandP_metadata()
+test_SandP_metadata()
 
 print("Testing company metadata api")
 data = intrinio_get_company_metadata('GE')
@@ -13,3 +19,14 @@ print(data)
 print("Testing company financials api")
 data = intrinio_get_company_financials('GE', '2010', 'FY')
 print(data)
+
+
+
+print("Testing company financials api via CSV")
+data = intrinio_get_company_financials_csv('GE', '2010', 'FY')
+
+print data
+my_list = list(data)
+print my_list
+for row in my_list:
+	print(row)
--- metadata/md_intrinio_client.py
+++ metadata/md_intrinio_client.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[4]:
 
 from api_key import INTRINIO_USERNAME
 from api_key import INTRINIO_PASSWORD                 
@@ -12,13 +12,13 @@ import json
 import requests
 
 
-# In[3]:
-
-print (INTRINIO_USERNAME)
-print(INTRINIO_PASSWORD)
-
-
 # In[5]:
+
+#print (INTRINIO_USERNAME)
+#print(INTRINIO_PASSWORD)
+
+
+# In[6]:
 
 def intrinio_get_company_metadata(symbol):
         # Get the latest FY Income Statement for "symbol"
@@ -37,11 +37,11 @@ def intrinio_get_company_metadata(symbol):
         data = response.json()
         return(json.dumps(data, indent=1, sort_keys=True))
 
-data = intrinio_get_company_metadata('GE')
-print(data)
+#data = intrinio_get_company_metadata('GE')
+#print(data)
 
 
-# In[11]:
+# In[7]:
 
 def intrinio_get_company_financials(symbol, year, quarter):
         # Get the latest FY Income Statement for "symbol"
@@ -72,10 +72,44 @@ def intrinio_get_company_financials(symbol, year, quarter):
                 #print(tag + ": " + str(value))
         return (json.dumps(cleanedupdata, indent=1, sort_keys=True))
 
-data = intrinio_get_company_financials('GE', '2010', 'FY')
+#data = intrinio_get_company_financials('GE', '2010', 'FY')
 
 
-# In[7]:
+# In[18]:
+
+intrinio_get_company_financials("GE", "2014", "Q3")
+
+
+# In[12]:
+
+import csv
+
+def intrinio_get_company_financials_csv(symbol, year, quarter):
+        # Get the latest FY Income Statement for "symbol"
+
+        cleanedupdata = {}
+        base_url = "https://api.intrinio.com"
+        request_url = base_url + "/financials/standardized.csv?"
+        query_params = {
+                'ticker': symbol,
+                'statement': 'income_statement',
+                'fiscal_year' : str(year),
+                'fiscal_period' : str(quarter),
+                'type': 'FY'
+        }
+
+        response = requests.get(request_url, params=query_params, auth=(INTRINIO_USERNAME, INTRINIO_PASSWORD))
+        if response.status_code == 401: print("Unauthorized! Check your username and password."); exit()
+
+        return(response)
+
+
+# In[16]:
+
+intrinio_get_company_financials_csv("GE", "2006", "Q3")
+
+
+# In[5]:
 
 import sys
 import json
@@ -88,36 +122,55 @@ sp500 = symbols.get_sp500_symbols()
 companies = json.dumps(sp500, indent=1, sort_keys=True)
 
 
-# In[8]:
+# In[6]:
 
-import os
-count = 0
-def get_SandP_metadat():
-    if not os.path.exists("data"):
-        os.makedirs("data")
-    for entry in sp500:
-        datafileName = "./data/data_" + str(entry["symbol"])
-        with open(datafileName, "w") as outfile:
-            data = intrinio_get_company_metadata(entry["symbol"])
-            json.dump(data, outfile, indent=1, sort_keys=True)
-            count += 1
-
-
-# In[9]:
-
-# Reading data back
-with open('data/data_MMM', 'r') as f:
-     data = json.load(f)
-
-print(data)
+# import os
+# count = 0
+# def get_SandP_metadata():
+#     if not os.path.exists("data"):
+#         os.makedirs("data")
+#     for entry in sp500:
+#         datafileName = "./data/data_" + str(entry["symbol"])
+#         with open(datafileName, "w") as outfile:
+#             data = intrinio_get_company_metadata(entry["symbol"])
+#             json.dump(data, outfile, indent=1, sort_keys=True)
+#             count += 1
 
 
-# In[10]:
+# In[7]:
+
+# # Reading data back
+# with open('data/data_MMM', 'r') as f:
+#      data = json.load(f)
+
+# print(data)
+
+
+# In[ ]:
 
 #!jupyter nbconvert --to script md_intrinio_client.ipynb
 
 
 # In[ ]:
 
+import os
+def get_SandP_metadata():
+    count = 0
+    if not os.path.exists("data"):
+        os.makedirs("data")
+    for entry in sp500:
+        datafileName = "./data/data_" + str(entry["symbol"]+".json")
+        with open(datafileName, "w") as outfile:
+            data = intrinio_get_company_metadata(entry["symbol"])
+            json.dump(data, outfile, indent=1, sort_keys=True)
+            count += 1
+
+
+# Reading data back
+def test_SandP_metadata():
+        with open('data/data_MMM.json', 'r') as f:
+                data = json.load(f)
+
+        print(data)
 
 
